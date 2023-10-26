4fb2d2d558146cac95ee329c35e92ff687ba61f4
spsharman <spsharman@gmail.com>
2023-10-26 15:55:16 +0100

    tn-ajshahid

---
+++ tn-ajshahid/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "CiscoDevNet/aci"
+    }
+  }
+}
+
+provider "aci" {
+  username = "username"
+  password = "password"
+  url      = "https://apic.url"
+}
+
+module "aci" {
+  source  = "netascode/nac-aci/aci"
+  version = "0.7.0"
+
+  yaml_directories = ["data"]
+
+  manage_access_policies    = false
+  manage_fabric_policies    = false
+  manage_pod_policies       = false
+  manage_node_policies      = false
+  manage_interface_policies = false
+  manage_tenants            = true
+}
