b7cc783c4b0128c0b05fd305ec6b168974a7a155
spsharman <spsharman@gmail.com>
2024-05-23 16:11:05 +0100

    new access policy model

---
+++ access-policies-new-model/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "CiscoDevNet/aci"
+      version = ">=2.13.2"
+    }
+  }
+}
+
+provider "aci" {
+  username = var.apic_username
+  password = var.apic_password
+  url      = var.apic_url
+}
+
+module "aci" {
+    # source = "github.com/netascode/terraform-aci-nac-aci?ref=main"
+  source  = "netascode/nac-aci/aci"
+  version = ">=0.8.1"
+
+  yaml_directories = ["data"]
+
+  manage_access_policies    = true
+  manage_fabric_policies    = false
+  manage_pod_policies       = false
+  manage_node_policies      = true
+  manage_interface_policies = true
+  manage_tenants            = false
+}
