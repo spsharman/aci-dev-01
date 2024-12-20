0ecee928cc6b48be0143417bfffc1d5cd2f1edd6
spsharman <spsharman@gmail.com>
2024-12-20 16:23:52 +0000

    add transit routing

--- tn-shared-services/main.tf
+++ tn-shared-services/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aci = {
       source = "CiscoDevNet/aci"
-      version = ">=2.13.2"
+      # version = ">=2.13.2"
     }
   }
 }
@@ -15,10 +15,10 @@ provider "aci" {
 
 module "aci" {
   source  = "netascode/nac-aci/aci"
-  version = ">=0.8.1"
+  # version = ">=0.8.1"
 
   # yaml_directories = ["data"]
-  yaml_directories = ["data", "data/code-testing-01"]
+  yaml_directories = ["data/code-testing-01"]
 
   manage_access_policies    = false
   manage_fabric_policies    = false
