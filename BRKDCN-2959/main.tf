82d5d86c729b9e1a479d7e01fe5b3a3ede998ec1
spsharman <spsharman@gmail.com>
2025-05-06 13:55:08 +0100

    updates for BRKDCN-2959

--- BRKDCN-2959/main.tf
+++ BRKDCN-2959/main.tf
@@ -18,22 +18,22 @@ module "aci" {
 
   # yaml_directories = ["data"] # to exclude a set of configuration move the .nac.yaml file to the /data/excluded directory
 
-  # yaml_files = ["data/01-production-current-design-l3out-consumer.nac.yaml"]
-  # yaml_files = ["data/02-production-current-design-l3out-provider.nac.yaml"]
-  # yaml_files = ["data/03-production-dual-vrf-design-l3out-consumer.nac.yaml"]  
-  # yaml_files = ["data/04-production-dual-vrf-design-l3out-provider.nac.yaml"]
-  # yaml_files = ["data/05-production-dual-vrf-invalid-design-l3out-consumer-vzAny-provider.nac.yaml"]
-  # yaml_files = ["data/06-production-shared-l3out-design-l3out-consumer.nac.yaml"]
-  # yaml_files = ["data/07-production-shared-l3out-design-l3out-provider.nac.yaml"]
-  # yaml_files = ["data/08-production-dual-vrf-design-in-common-l3out-consumer.nac.yaml"]
-  # yaml_files = ["data/09-production-dual-vrf-design-in-common-l3out-provider.nac.yaml"]
-  # yaml_files = ["data/10-production-shared-l3out-design-l3out-provider-ssh-to-vzAny.nac.yaml"]
-  # yaml_files = ["data/11-production-shared-l3out-design-l3out-consumer-consolidated-BD-net-centric.nac.yaml"]
-  # yaml_files = ["data/12-production-shared-l3out-design-l3out-consumer-consolidated-BD-app-centric.nac.yaml"]
-  # yaml_files = ["data/13-production-single-vrf-design-esg-l3out-consumer.nac.yaml"]
-  # yaml_files = ["data/14-production-shared-l3out-design-esg-l3out-consumer.nac.yaml"]
+  # yaml_files = ["data/01-production-current-design-vzAny-allows-open-communication.nac.yaml"]
+  # yaml_files = ["data/02-production-dual-vrf-design-l3out-consumer.nac.yaml"]  
+  # yaml_files = ["data/03-production-dual-vrf-design-l3out-provider.nac.yaml"]
+  # yaml_files = ["data/04-production-dual-vrf-invalid-design-l3out-consumer-vzAny-provider.nac.yaml"]
+  # yaml_files = ["data/05-production-shared-l3out-design-l3out-consumer.nac.yaml"]
+  # yaml_files = ["data/06-production-shared-l3out-design-l3out-provider.nac.yaml"]
+  # yaml_files = ["data/07-production-dual-vrf-design-in-common-l3out-consumer.nac.yaml"]
+  # yaml_files = ["data/08-production-dual-vrf-design-in-common-l3out-provider.nac.yaml"]
+  # yaml_files = ["data/09-production-shared-l3out-design-l3out-provider-ssh-to-vzAny.nac.yaml"]
+  # yaml_files = ["data/10-production-shared-l3out-design-l3out-consumer-consolidated-BD-net-centric.nac.yaml"]
+  # yaml_files = ["data/11-production-shared-l3out-design-l3out-consumer-consolidated-BD-app-centric.nac.yaml"]
+  # yaml_files = ["data/12-production-single-vrf-design-esg-l3out-consumer.nac.yaml"]
+  # yaml_files = ["data/13-production-shared-l3out-design-esg-l3out-consumer.nac.yaml"]
+  yaml_files = ["data/14-production-shared-l3out-design-esg-l3out-consumer-with-vzAny.nac.yaml"]
   # yaml_files = ["data/15-production-shared-l3out-design-with-fw-esg-l3out-consumer.nac.yaml"]
-  yaml_files = ["data/16-production-dual-l3out-design-with-fw-esg-l3out-consumer.nac.yaml"]
+  # yaml_files = ["data/16-production-dual-l3out-design-with-fw-esg-l3out-consumer.nac.yaml"]
 
 
   manage_access_policies    = false
