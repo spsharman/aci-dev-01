99d3e769376eaf8a492a4ec06d5f9a159f8bb07d
spsharman <spsharman@gmail.com>
2025-10-09 14:23:57 +0100

    isovalent migration to new subnet

--- tn-isovalent/main.tf
+++ tn-isovalent/main.tf
@@ -20,8 +20,8 @@ module "aci" {
   # yaml_directories = ["svi"]
   # yaml_directories = ["floating-svi"]
   # yaml_files = ["svi/configuration.nac.yaml"]
-  yaml_files = ["floating-svi/configuration-v3.nac.yaml"]
-
+  # yaml_files = ["floating-svi/configuration-v3.nac.yaml"]
+  yaml_files = ["floating-svi/configuration-v4.nac.yaml"]
 
   manage_access_policies    = false
   manage_fabric_policies    = false
