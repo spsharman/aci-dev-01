6b6e1c4897f11445a11bc5863bb4833adbf8ad51
spsharman <spsharman@gmail.com>
2026-07-09 17:18:16 +0100

    Add tech-elevate VRF configuration and BGP policies for enhanced network management

--- tn-tech-elevate/main.tf
+++ tn-tech-elevate/main.tf
@@ -18,7 +18,7 @@ module "aci" {
   
   # source = "github.com/netascode/terraform-aci-nac-aci?ref=main"
 
-  # yaml_files = ["bgp-applied-to-node/00-base-network.nac.yaml"]
+  yaml_files = ["bgp-applied-to-node/00-base-network.nac.yaml"]
   # yaml_files = ["bgp-applied-to-node/01-add-split-vrf.nac.yaml"]
   # yaml_files = ["bgp-applied-to-node/02-add-route-leaking.nac.yaml"]    
   # yaml_files = ["bgp-applied-to-node/03-epg-to-esg-mapping.nac.yaml"]   
@@ -27,7 +27,7 @@ module "aci" {
   # yaml_files = ["bgp-applied-to-node/06-add-ftdv-east-west-flows.nac.yaml"]   
   # yaml_files = ["bgp-applied-to-node/07-add-contract-to-each-esg.nac.yaml"]
   # yaml_files = ["bgp-applied-to-node/08-add-application-esgs-and-contracts.nac.yaml"]
-  yaml_files = ["bgp-applied-to-node/09-add-intra-application-contracts.nac.yaml"]
+  # yaml_files = ["bgp-applied-to-node/09-add-intra-application-contracts.nac.yaml"]
   
   
   manage_access_policies    = false
